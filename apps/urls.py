8388d14a6df5cac14fa3d2bd3758184e3c9cbc77
likem3 <islike1221@gmail.com>
2023-07-06 20:12:45 +0800

    add handler api currency detail

--- apps/urls.py
+++ apps/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from apps.views import (
     CurrencyListView,
+    CurrencyDetailView,
     AddressRetrieveView
 )
 
 
 urlpatterns = [
     path('currencies/', CurrencyListView.as_view()),
+    path('currencies/<int:pk>/', CurrencyDetailView.as_view()),
     path('address/', AddressRetrieveView.as_view()),
 ]
--- apps/views.py
+++ apps/views.py
@@ -7,6 +7,10 @@ class CurrencyListView(generics.ListAPIView):
     serializer_class = CurrencySerializer
     queryset = Currency.objects.filter(status='active')
 
+class CurrencyDetailView(generics.RetrieveAPIView):
+    serializer_class = CurrencySerializer
+    queryset = Currency.objects.filter(status='active')
+
 
 class AddressRetrieveView(generics.CreateAPIView):
     serializer_class = AddressSerializer
