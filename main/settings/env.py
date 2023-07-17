e9f2c101c33a24b517e24593c856dc2cca117d14
likem3 <islike1221@gmail.com>
2023-07-17 11:10:26 +0800

    handle static file 404 and some settings adjustment

--- main/settings/env.py
+++ main/settings/env.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 ENVIRONMENT_SETTING = os.getenv('ENVIRONMENT_SETTING')
 
 if ENVIRONMENT_SETTING not in ['local', 'development', 'production']:
--- main/settings/settings.py
+++ main/settings/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p$h&8^ca(b3un@$egl4g38)_r&nw8ez_bs#h+w0b)l0a+f6gnf'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -122,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
